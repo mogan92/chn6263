1ca3d48fe72a7aac55fefa3e7311a9c157132660
matthew <matt@yahoo.com>
2023-05-17 17:03:52 +0530

    program2 commit

---
+++ forloop.py
@@ -0,0 +1,9 @@
+# for , while
+
+mylist = [10,21,35,42,58]
+
+for i in mylist:        #value = 1 , value = 2 , value = 3 , value = 4
+    if i % 2 == 0:
+        print(i,"is even")
+    else:
+        print(i,"is odd")
