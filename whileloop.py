d2beaab673aaaadf9dea3746fc6cff1528b8d842
matthew <matt@yahoo.com>
2023-05-17 16:44:32 +0530

    my first commit

---
+++ whileloop.py
@@ -0,0 +1,10 @@
+# while loop
+
+i = 0
+
+while i < 10:           # i = 0 , 0<10 , i =1 , 1<10
+    if i % 2 == 0:
+        print(i,"even")
+    else:
+        print(i,"odd")  
+    i = i + 1           #1,2
